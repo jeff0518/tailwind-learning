02801e993094557faba2d075b24e577846f4d145
jeff.wang <jeff.wang@collaborate.tw>
2025-01-24 14:25:43 +0800

    feat: add Header

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,9 +1,17 @@
-const Button = ({ children }) => {
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+const Button = ({ children }: ButtonProps) => {
   return (
     <>
-        <button className="" href="">{children}</button>
+      <button className="rounded-full border-2 bg-blue-700 px-12 py-3 text-lg text-white hover:bg-transparent hover:text-indigo-600">
+        {children}
+      </button>
     </>
-  )
-}
+  );
+};
 
 export default Button;
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -2,11 +2,21 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <header>
-      <h1>My React App</h1>
-      <Button>Click Me</Button>
+    <header className="bg-gray-800 text-white">
+      <div className="px-6 py-3 flex flex-row justify-between mx-auto container">
+        <div className="flex items-center">
+          <a href="#" className="text-3xl font-medium">
+            Logo
+          </a>
+        </div>
+        <div>
+          <Button>Click Me</Button>
+          <Button>Click Me</Button>
+          <Button>Click Me</Button>
+        </div>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
